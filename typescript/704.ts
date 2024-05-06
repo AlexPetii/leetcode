fa83427d2593a09aa40df7f700fe117c78b71f09
AlexPet <alexandrpet195@gmail.com>
2024-05-06 22:01:32 +0300

    problem 704

---
+++ typescript/704.ts
@@ -0,0 +1,17 @@
+function search(nums: number[], target: number): number {
+    let low = 0;
+    let high = nums.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = nums[mid];
+        if (guess == target) {
+            return mid;
+        }
+        if (guess > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+};
