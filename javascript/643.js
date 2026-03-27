9a444afcd7f80dab9a20a08b2ac61eb3d50a34b7
AlexPet <alexandrpet195@gmail.com>
2026-03-27 19:43:46 +0100

    problem 643 sliding window

--- javascript/643.js
+++ javascript/643.js
@@ -1,17 +1,20 @@
-var findMaxAverage = function (nums, k) {
-  let maxSum = 0;
-  for (let i = 0; i < k; i++) {
-    maxSum += nums[i];
-  }
-
-  let currSum = maxSum;
-  for (let i = k; i < nums.length; i++) {
-    currSum += nums[i] - nums[i - k];
-    if (currSum > maxSum) {
-      maxSum = currSum;
-    }
-  }
-  return maxSum / k;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+  let begin = 0
+  let result = -Infinity
+  let window_state = 0
+  for(let end = 0; end < nums.length; end++){
+      window_state += nums[end]
+      if(end - begin + 1 === k){
+          result = Math.max(result, window_state)
+          window_state -= nums[begin]
+          begin++
+      }
+  }return result / k
 };
 
 console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4));
